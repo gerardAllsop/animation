92dae81807c4bb7d19711fe8f96be0574c6239c8
gja10 <g.j.allsop@brighton.ac.uk>
2018-10-23 09:29:00 +0100

    Clean up for week 4

--- desktop/src/com/allsopg/game/desktop/DesktopLauncher.java
+++ desktop/src/com/allsopg/game/desktop/DesktopLauncher.java
@@ -1,11 +1,8 @@
 package com.allsopg.game.desktop;
 
-import com.allsopg.game.BasicAnimation;
 import com.allsopg.game.ViewPortExample;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
-import com.allsopg.game.MyGdxGame;
-import com.badlogic.gdx.tools.texturepacker.TexturePacker;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
