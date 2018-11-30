a66f94673c1d6d5a886acf43447ec187ef0c118d
gja10 <g.j.allsop@brighton.ac.uk>
2018-11-30 15:42:42 +0000

    Added sound branch

---
+++ core/src/com/allsopg/game/SoundEffect.java
@@ -0,0 +1,54 @@
+package com.allsopg.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.InputProcessor;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.utils.IntMap;
+
+public class SoundEffect implements InputProcessor{
+    private IntMap<Sound> soundKeys;
+
+    public SoundEffect(){
+        soundKeys = new IntMap<Sound>();
+        Sound backSnd = Gdx.audio.newSound(Gdx.files.internal(""));
+    }
+    @Override
+    public boolean keyDown(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyUp(int keycode) {
+        return false;
+    }
+
+    @Override
+    public boolean keyTyped(char character) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDown(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchUp(int screenX, int screenY, int pointer, int button) {
+        return false;
+    }
+
+    @Override
+    public boolean touchDragged(int screenX, int screenY, int pointer) {
+        return false;
+    }
+
+    @Override
+    public boolean mouseMoved(int screenX, int screenY) {
+        return false;
+    }
+
+    @Override
+    public boolean scrolled(int amount) {
+        return false;
+    }
+}
