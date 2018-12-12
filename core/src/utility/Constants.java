61e8ec94f3b44c033217799ee113f343e78892cd
allsopg <g.j.allsop@brighton.ac.uk>
2018-12-12 11:59:10 +0000

    Pull check from C206

--- core/src/utility/Constants.java
+++ core/src/utility/Constants.java
@@ -7,4 +7,5 @@ public class Constants {
     public static final String SIZE = "data/smallSize.png";
     public static final String SAW_ATLAS = "texture_atlas/saw_assets.atlas";
     public static final String BALL_ATLAS = "texture_atlas/ball_assets.atlas";
+    //Added 11:25, 12/12/2018 from Room 206
 }
